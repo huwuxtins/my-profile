2a219ccd65e1ede0004b875e6ea0549ba0e57b68
huwuxtins <nguyenhuutin124@gmail.com>
2024-07-18 00:46:13 +0700

    front-end: add interaction api

---
+++ frontend/src/api/interaction.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const addFollow = (followerID, targetID) => {
+
+}
+
+const unFollow = (followerID, targetID) => {
+
+}
+
+const addFriend = (userID, friendID) => {
+
+}
+
+const unFriend = (userID, friendID) => {
+
+}
+
+export { addFollow, unFollow, addFriend, unFriend }
