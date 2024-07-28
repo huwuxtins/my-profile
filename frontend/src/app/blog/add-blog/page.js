e8bb63d2396e30437e86e3c2caf7f1b75ae45307
huwuxtins <nguyenhuutin124@gmail.com>
2024-07-28 22:30:51 +0700

    front-end: blog page

--- frontend/src/app/blog/add-blog/page.js
+++ frontend/src/app/blog/add-blog/page.js
@@ -4,20 +4,32 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { compress, decompress } from 'lz-string';
+import { addBlog } from '~/api/blog';
+import { v4 as uuidv4 } from 'uuid';
 
 function AddBlogPage() {
     const [value, setValue] = useState('');
+
+    const [isSaved, setIsSaved] = useState(0)
 
     const handleChange = (value, editor) => {
         setValue(value)
     }
 
-    const handleSave = (value) => {
+    const handleSave = async (blogTitle, value) => {
         alert("Size of sample is: " + value.length);
         var compressed = compress(string);
         alert("Size of compressed sample is: " + compressed.length);
         var string = decompress(compressed);
         alert("Sample is: " + string);
+
+        const blog = await addBlog({
+            id: uuidv4(),
+            createdAt: Date.now(),
+            title: blogTitle,
+            content: value,
+
+        })
     }
 
     const modules = {
@@ -64,15 +76,22 @@ function AddBlogPage() {
         <div className='flex flex-row justify-between items-center'>
             <span className='text-4xl font-bold my-10 text-gray-800 dark:text-gray-200'>Add blog</span>
         </div>
+        {isSaved != 0 ? <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong className="font-bold">{isSaved > 0? 'Success': 'Failed'}</strong>
+            <span className="block sm:inline">{isSaved > 0? 'Blog is saved successfully!': 'Saving this blog failed, please check and try again!'}</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg className="fillCurrent h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+            </span>
+        </div> : null}
         <div className='flex flex-col lg:flex-row justify-between'>
             <ReactQuill theme="snow"
                 value={value}
                 onChange={(value, delta, source, editor) => handleChange(value, editor)}
-                className='border-2 overflow-hidden rounded-lg lg:basis-1/2 mb-5 lg:mb-0 lg:mr-5' style={{height: '550px'}}
+                className='border-2 overflow-hidden rounded-lg lg:basis-1/2 mb-5 lg:mb-0 lg:mr-5' style={{ height: '550px' }}
                 modules={modules}
                 formats={formats}
             />
-            <div className='flex flex-col border-2 rounded-lg lg:basis-1/2' style={{height: '550px'}}>
+            <div className='flex flex-col border-2 rounded-lg lg:basis-1/2' style={{ height: '550px' }}>
                 <span className='items-center border-t-2 border p-5 font-bold'>
                     Preview
                 </span>
