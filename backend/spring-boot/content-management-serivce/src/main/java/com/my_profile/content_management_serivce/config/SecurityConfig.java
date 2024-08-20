bfa0e4429777128d3f7fa54afd27dff580b1c37d
huwuxtins <nguyenhuutin124@gmail.com>
2024-08-21 00:08:58 +0700

    spring-boot: update security

--- backend/spring-boot/content-management-serivce/src/main/java/com/my_profile/content_management_serivce/config/SecurityConfig.java
+++ backend/spring-boot/content-management-serivce/src/main/java/com/my_profile/content_management_serivce/config/SecurityConfig.java
@@ -1,12 +1,23 @@
 package com.my_profile.content_management_serivce.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -15,6 +26,21 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Value("spring.security.oauth2.client.registration.auth0.client-id")
+    private String clientID;
+
+    @Value("spring.security.oauth2.client.registration.auth0.client-secret")
+    private String clientSecret;
+
+    @Value("spring.security.oauth2.client.registration.auth0.authorization-grant-type")
+    private String authorizationGrantType;
+
+    @Value("spring.security.oauth2.client.provider.auth0.issuer-uri")
+    private String issuerUri;
+
+    @Value("spring.security.oauth2.client.registration.auth0.scope")
+    private String scope;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -35,13 +61,46 @@ public class SecurityConfig {
         return ReactiveJwtDecoders.fromIssuerLocation("https://dev-k6vjpfkbkgmdsry6.us.auth0.com/");
     }
 
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        ClientRegistration auth0Registration = ClientRegistration.withRegistrationId("auth0")
+//                .clientId(clientID)
+//                .clientSecret(clientSecret)
+//                .scope(scope)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .issuerUri(issuerUri)
+//                .build();
+//
+//        return new InMemoryClientRegistrationRepository(auth0Registration);
+//    }
+
     @Bean
-    WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
-                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2Client.setDefaultOAuth2AuthorizedClient(true);
+    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+                ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .clientCredentials()
+                        .password()
+                        .build();
+
+        DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultReactiveOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+    @Bean
+    public WebClient webClient(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
         return WebClient.builder()
-                .apply(oauth2Client.oauth2Configuration())
+                .filter(oauth2Client)
                 .build();
     }
 }
