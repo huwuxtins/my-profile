20da53f3ed0b32a702614bdecc9ac43c364c9076
huwuxtins <nguyenhuutin124@gmail.com>
2024-10-31 23:08:51 +0700

    spring-boot: add notification class

---
+++ backend/spring-boot/notification-service/src/main/java/com/my_profile/notification_service/model/Notification.java
@@ -0,0 +1,30 @@
+package com.my_profile.notification_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+    @Id
+    private String id;
+
+    private String title;
+
+    @Field("created_at")
+    private Date createdAt;
+
+    @Field("updated_at")
+    private Date updatedAt;
+    private String content;
+
+    @Field("user_ids")
+    private List<String> userIDs;
+}
