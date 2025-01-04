818bfa5ee784ba05dd73ef9b655d6fcff009bcb9
huwuxtins <nguyenhuutin124@gmail.com>
2025-01-04 09:21:04 +0700

    update code

---
+++ backend/spring-boot/notification-service/src/main/java/com/my_profile/notification_service/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package com.my_profile.notification_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/notification")
+public class NotificationController {
+
+//    private final
+}
