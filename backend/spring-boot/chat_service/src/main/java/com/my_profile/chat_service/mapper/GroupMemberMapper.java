767a7f7a0706bd577a53025a5d1f3965789ce05f
huutin <nguyenhuutin124@gmail.com>
2026-01-15 17:37:27 +0700

    feat: update GroupMemberMapper and MessageMapper chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/mapper/GroupMemberMapper.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/mapper/GroupMemberMapper.java
@@ -4,4 +4,6 @@ import com.my_profile.chat_service.entity.GroupMember;
 import com.my_profile.chat_service.mapper.dto.GroupMemberDto;
 
 public interface GroupMemberMapper extends BaseMapper<GroupMemberDto, GroupMember>{
+    GroupMemberDto toDto(GroupMember groupMember);
+    GroupMember toEntity(GroupMemberDto dto);
 }
--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/mapper/MessageMapper.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/mapper/MessageMapper.java
@@ -1,4 +1,9 @@
 package com.my_profile.chat_service.mapper;
 
-public interface MessageMapper {
+import com.my_profile.chat_service.entity.Message;
+import com.my_profile.chat_service.mapper.dto.MessageDto;
+
+public interface MessageMapper extends BaseMapper<MessageDto, Message>{
+    MessageDto toDto(Message message);
+    Message toEntity(MessageDto dto);
 }
