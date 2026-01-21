abf88f1bc5c038de1a4d06850fbf272e8f19b732
huutin <nguyenhuutin124@gmail.com>
2026-01-21 22:48:03 +0700

    feat: add cache and security chat-service

---
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/config/FeignClientConfiguration.java
@@ -0,0 +1,33 @@
+package com.my_profile.chat_service.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+
+//Config for call api to protected client api
+@Configuration
+public class FeignClientConfiguration {
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+        return requestTemplate -> {
+            OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                    .withClientRegistrationId("auth0")
+                    .principal("feign-client")
+                    .attributes(attrs -> {
+                        attrs.put("audience", "https://dev-k6vjpfkbkgmdsry6.us.auth0.com/api/v2/"); // Add the audience parameter
+                    })
+                    .build();
+
+            OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+            if (authorizedClient != null) {
+                OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+                requestTemplate.header("Authorization", "Bearer " + accessToken.getTokenValue());
+            }
+        };
+    }
+}
---
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.my_profile.chat_service.config;
+
+import com.my_profile.chat_service.security.Auth0ClientCredentialsGrantRequestEntityConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.RequestEntity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.endpoint.DefaultClientCredentialsTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            .requestMatchers("/", "/images/**", "/api/v1/content-management-swagger/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("https://dev-k6vjpfkbkgmdsry6.us.auth0.com/");
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        Converter<OAuth2ClientCredentialsGrantRequest, RequestEntity<?>> customRequestEntityConverter = new Auth0ClientCredentialsGrantRequestEntityConverter("https://dev-k6vjpfkbkgmdsry6.us.auth0.com/api/v2/");
+
+        var authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .refreshToken()
+                        .clientCredentials(clientCredentialsGrantBuilder -> {
+                            var clientCredentialsTokenResponseClient = new DefaultClientCredentialsTokenResponseClient();
+                            clientCredentialsTokenResponseClient.setRequestEntityConverter(customRequestEntityConverter);
+                            clientCredentialsGrantBuilder.accessTokenResponseClient(clientCredentialsTokenResponseClient);
+                        })
+                        .build();
+
+        var authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+}
