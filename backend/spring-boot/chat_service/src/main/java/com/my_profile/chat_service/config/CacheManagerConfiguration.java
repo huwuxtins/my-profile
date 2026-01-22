e8391d7e68ac691bbaa366ae2b0664f97ec76c37
huutin <nguyenhuutin124@gmail.com>
2026-01-22 12:00:32 +0700

    feat: downgrade version 4 to 3 chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/config/CacheManagerConfiguration.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/config/CacheManagerConfiguration.java
@@ -1,32 +1,32 @@
-//package com.my_profile.chat_service.config;
-//
-//import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
-//import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-//import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.cache.RedisCacheConfiguration;
-//import org.springframework.data.redis.cache.RedisCacheManager;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//
-//import java.time.Duration;
-//
-//@Configuration(proxyBeanMethods = false)
-//public class CacheManagerConfiguration {
-//    @Bean
-//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-//        return RedisCacheManager.create(connectionFactory);
-//    }
-//
-//    @Bean
-//    public CacheManagerCustomizer<ConcurrentMapCacheManager> cacheManagerCacheManagerCustomizer() {
-//        return (cacheManager) -> cacheManager.setAllowNullValues(false);
-//    }
-//
-//    @Bean
-//    public RedisCacheManagerBuilderCustomizer myRedisCacheManagerBuilderCustomizer() {
-//        return (builder) -> builder
-//                .withCacheConfiguration("cache1", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(10)))
-//                .withCacheConfiguration("cache2", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMillis(1)));
-//    }
-//}
+package com.my_profile.chat_service.config;
+
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration(proxyBeanMethods = false)
+public class CacheManagerConfiguration {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.create(connectionFactory);
+    }
+
+    @Bean
+    public CacheManagerCustomizer<ConcurrentMapCacheManager> cacheManagerCacheManagerCustomizer() {
+        return (cacheManager) -> cacheManager.setAllowNullValues(false);
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer myRedisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("cache1", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(10)))
+                .withCacheConfiguration("cache2", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMillis(1)));
+    }
+}
