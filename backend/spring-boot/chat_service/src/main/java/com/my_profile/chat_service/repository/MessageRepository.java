0b4e26019ab7062496d92bb2d4c4b4ab458ecbf2
huutin <nguyenhuutin124@gmail.com>
2026-01-23 15:48:30 +0700

    feat: add findMsgByContent chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/repository/MessageRepository.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/repository/MessageRepository.java
@@ -4,6 +4,8 @@ import com.my_profile.chat_service.entity.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -11,4 +13,24 @@ import java.util.UUID;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, UUID> {
     Page<Message> findByGroupId(UUID id, Pageable pageable);
+
+    @Query(
+            value = """
+        SELECT *
+        FROM messages m
+        WHERE to_tsvector('english', m.content)
+              @@ plainto_tsquery('english', :query)
+        """,
+            countQuery = """
+        SELECT COUNT(*)
+        FROM messages m
+        WHERE to_tsvector('english', m.content)
+              @@ plainto_tsquery('english', :query)
+        """,
+            nativeQuery = true
+    )
+    Page<Message> findMsgByContent(
+            @Param("query") String query,
+            Pageable pageable
+    );
 }
