9b0991cb84c10c2b37dc7afe49dcdf15e2dbc86a
huutin <nguyenhuutin124@gmail.com>
2026-01-26 17:11:57 +0700

    feat: add attachment controller chat-service

---
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/controller/MessageAttachmentController.java
@@ -0,0 +1,16 @@
+package com.my_profile.chat_service.controller;
+
+import com.my_profile.chat_service.service.MessageAttachmentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/message-attachment")
+public class MessageAttachmentController {
+
+    private final MessageAttachmentService attachmentService;
+
+    public MessageAttachmentController(MessageAttachmentService attachmentService) {
+        this.attachmentService = attachmentService;
+    }
+}
