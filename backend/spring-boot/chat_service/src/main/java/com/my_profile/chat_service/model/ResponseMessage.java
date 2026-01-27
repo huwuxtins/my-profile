d303becc1cbfe1ccf35ebd93e892316b0c4d8701
huutin <nguyenhuutin124@gmail.com>
2026-01-27 12:19:26 +0700

    feat: update ResponseMessage chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/model/ResponseMessage.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/model/ResponseMessage.java
@@ -1,11 +1,10 @@
 package com.my_profile.chat_service.model;
 
-import com.my_profile.content_management_serivce.model.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseMessage {
-    public static <E> ResponseEntity<ApiResponse<E>> createResponse(String message, E data, HttpStatus status){
+    public static <E> ResponseEntity<com.my_profile.chat_service.model.ApiResponse<E>> createResponse(String message, E data, HttpStatus status){
         ApiResponse<E> response = new ApiResponse<E>(message, data);
         return new ResponseEntity<>(response, status);
     }
