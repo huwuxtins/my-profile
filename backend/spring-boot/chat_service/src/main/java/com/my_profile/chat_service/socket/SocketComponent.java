a5a93c71a6a593ae3781769280f87e2e0e323a1a
huutin <nguyenhuutin124@gmail.com>
2026-02-04 14:19:17 +0700

    feat: update socket server and socket component - chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/socket/SocketComponent.java
+++
@@ -1,4 +0,0 @@
-package com.my_profile.chat_service.socket;
-
-public class SocketComponent {
-}
--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/socket/SocketServer.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/socket/SocketServer.java
@@ -1,4 +1,22 @@
 package com.my_profile.chat_service.socket;
 
+import com.corundumstudio.socketio.SocketIOServer;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@SuppressWarnings("unused")
 public class SocketServer {
+    private final SocketIOServer server;
+
+    public SocketServer(SocketIOServer server) {
+        this.server = server;
+    }
+
+    @PostConstruct
+    public void initListeners(){
+
+    }
 }
