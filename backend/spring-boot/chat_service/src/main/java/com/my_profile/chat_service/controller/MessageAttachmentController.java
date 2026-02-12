9bfc15f8684577e04ffc1f95f27b88a1ed4e7adf
huutin <nguyenhuutin124@gmail.com>
2026-02-12 09:36:16 +0700

    feat: add get attachment by messageId api - chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/controller/MessageAttachmentController.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/controller/MessageAttachmentController.java
@@ -1,8 +1,15 @@
 package com.my_profile.chat_service.controller;
 
+import com.my_profile.chat_service.mapper.dto.MessageAttachmentDto;
+import com.my_profile.chat_service.model.ApiResponse;
+import com.my_profile.chat_service.model.ResponseMessage;
 import com.my_profile.chat_service.service.MessageAttachmentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/message-attachment")
@@ -13,5 +20,13 @@ public class MessageAttachmentController {
 
     public MessageAttachmentController(MessageAttachmentService attachmentService) {
         this.attachmentService = attachmentService;
+    }
+
+    @GetMapping("messageId")
+    public ResponseEntity<ApiResponse<List<MessageAttachmentDto>>> getByMessageId(@PathVariable UUID messageId,
+                                                                                  @RequestParam(defaultValue = "0") int page,
+                                                                                  @RequestParam(defaultValue = "10") int size) {
+        List<MessageAttachmentDto> dto = this.attachmentService.findByMessageId(messageId);
+        return ResponseMessage.createResponse("Get attachment by messageId successfully!", dto, HttpStatus.OK);
     }
 }
