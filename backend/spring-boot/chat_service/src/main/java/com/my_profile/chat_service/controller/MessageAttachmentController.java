00bfd9ec7b41b37c49d644fe89374b7b889f7f5a
huutin <nguyenhuutin124@gmail.com>
2026-02-14 20:20:09 +0700

    feat: add delete attachment by id api - chat-service

--- backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/controller/MessageAttachmentController.java
+++ backend/spring-boot/chat_service/src/main/java/com/my_profile/chat_service/controller/MessageAttachmentController.java
@@ -1,5 +1,6 @@
 package com.my_profile.chat_service.controller;
 
+import com.my_profile.chat_service.exception.AccessDbException;
 import com.my_profile.chat_service.mapper.dto.MessageAttachmentDto;
 import com.my_profile.chat_service.model.ApiResponse;
 import com.my_profile.chat_service.model.ResponseMessage;
@@ -22,11 +23,17 @@ public class MessageAttachmentController {
         this.attachmentService = attachmentService;
     }
 
-    @GetMapping("messageId")
+    @GetMapping("/{messageId}")
     public ResponseEntity<ApiResponse<List<MessageAttachmentDto>>> getByMessageId(@PathVariable UUID messageId,
                                                                                   @RequestParam(defaultValue = "0") int page,
                                                                                   @RequestParam(defaultValue = "10") int size) {
         List<MessageAttachmentDto> dto = this.attachmentService.findByMessageId(messageId);
         return ResponseMessage.createResponse("Get attachment by messageId successfully!", dto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<String>> deleteMessage(@PathVariable UUID id) throws AccessDbException {
+        this.attachmentService.deleteAttachment(id);
+        return ResponseMessage.createResponse("Delete attachment by id successfully !", "Deleted", HttpStatus.OK);
     }
 }
